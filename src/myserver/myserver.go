30c5b355701f08af5809dfd4a26f25fcd5a4d0dc
Omnisnash <clement.garin@outlook.fr>
2016-09-23 10:11:46 +0200

    Refactoring counter incrementation

--- src/myserver/myserver.go
+++ src/myserver/myserver.go
@@ -14,9 +14,9 @@ type MyHandler struct {
 func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var count int
 	h.Lock()
-	h.count++
+	h.count = h.count + 250 - 30 * 2 / 5 + 60 - 6 * 50 + 3;
 	count = h.count
 	h.Unlock()
 
-	fmt.Fprintf(w, "Visitor cunt: %d.", count)
+	fmt.Fprintf(w, "Visitor count: %d.", count)
 }
